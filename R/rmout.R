7901259853d509931ec42d3d6d450c68466c15ac
osoramirez <oscar.ramirez.alan@una.ac.cr>
2019-02-04 08:26:24 -0600

    updates rmout

--- R/rmout.R
+++ R/rmout.R
@@ -1,34 +1,35 @@
 #'@title Remove outlier
 #'
-#'@param rmout Remove outlier
-#'@description Remove outlier.
+#'@description remove outlier
 #'@author Oscar Ramirez Alan (\email{osoramirez@@gmail.com}).
 #'@param x is a numeric value, could be a  a vector or data.frame
+#'@param na.rm A logical scalar. Should missing values
+#'@param ... Additional arguments
+#'@return rmout
 #'@export rmout
-#'@import stats
-#'@return a data set with oultiers
-#'@export
+#'@name rmout
 #'@examples
 #'set.seed(12345)
 #'x <- rnorm(100)
 #'x <- c(-15, x, 15)
 #'y <- rmout(x)
-#'par(mfrow = c(1, 2))
-#'boxplot(x)
-#'boxplot(y)
 
-
-rmout <- function(x, na.rm = TRUE) {
+rmout <- function(x, na.rm = TRUE,...) {
   x <- x[is.finite(x)]
+  na.rm = na.rm
   Quan <- quantile(x, probs=c(.25, .75), na.rm = na.rm)
   IQR <- 1.5 * IQR(x, na.rm = na.rm)
   y <- x
   y[x < (Quan[1] - IQR)] <- NA
   y[x > (Quan[2] + IQR)] <- NA
-  par(mfrow = c(1, 2))
-  boxplot(x,col="#EBEBEB", main="With outlier",  boxwex = .85, frame.plot = FALSE, cex=0.9, cex.lab=.9, cex.axis=.9, cex.main=.9)
+  def.par <- par(no.readonly = TRUE)
+  on.exit(par(def.par))
+  par(mfrow=c(1,2))
+  boxplot(x,col="#EBEBEB", main="With outlier",  boxwex = .85,
+          frame.plot = FALSE, cex=0.9, cex.lab=.9, cex.axis=.9, cex.main=.9,cex.main=.9, ylim = c(min(x),  max(x)))
   points(mean(x), pch=20, cex = 1.8)
-  boxplot(y,col="#EBEBEB", main="Without outlier", boxwex = .85, frame.plot = FALSE, cex=0.9, cex.lab=.9, cex.axis=.9, cex.main=.9)
-  points(mean(x), pch=20, cex = 1.8)
+  boxplot(y,col="#EBEBEB", main="With outlier",  boxwex = .85,
+          frame.plot = FALSE, cex=0.9, cex.lab=.9, cex.axis=.9, cex.main=.9,cex.main=.9, ylim = c(min(x),  max(x)))
+
 }
 
