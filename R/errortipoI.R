f13df59da87bce98a6a96ed76b4b6b42212270bb
osoramirez <oscar.ramirez.alan@una.ac.cr>
2019-02-19 17:23:17 -0600

    New

---
+++ R/errortipoI.R
@@ -0,0 +1,15 @@
+#'@title errortipoI
+#'
+#'@description del error tipo I en Estad
+#'@author Oscar Ramirez Alan (\email{osoramirez@@gmail.com}).
+#'@export errortipoI
+#'@keywords errortipoI
+#'@return errortipoI
+#'@export errortipoI
+#'@examples
+#'errortipoI()
+
+
+errortipoI <- function() {
+  print("Error de tipo alfa o falso positivo. Es cuando el investigador rechaza la hipotesis nula H0, siendo esta verdadera")
+}
---
+++ R/pvalue.R
@@ -0,0 +1,16 @@
+#'@title pvalue
+#'
+#'@description A pvalue.
+#'@author Oscar Ramirez Alan (\email{osoramirez@@gmail.com}).
+#'#'@export pvalue
+#'@keywords pvalue
+#'@return pvalue
+#'@export pvalue
+#'@examples
+#'pvalue()
+
+
+pvalue <- function() {
+  print("Representa la probabilidad de la ocurrencia de un evento dado. El p-value es utilizado para tener un criterio de aceptacion o rechazo de la hipotesis nula")
+}
+
